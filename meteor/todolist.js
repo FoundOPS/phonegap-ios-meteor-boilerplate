5aaa3adeab7915663d56b06af3ecd859ccec180e
Kaiwen Xu <xu227@purdue.edu>
2013-02-08 11:01:36 -0500

    update meteor template to todolist

---
+++ meteor/todolist.js
@@ -0,0 +1,37 @@
+Todos = new Meteor.Collection("todos");
+
+if (Meteor.isClient) {
+    Template.hello.greeting = function () {
+        return "Welcome to TodoList!";
+    };
+
+    Template.hello.todos = function () {
+        return Todos.find({});
+    };
+
+    Template.hello.events({
+        'click input.inc' : function () {
+            var input = document.getElementById("inputtext");
+            Todos.insert({item: input.value});
+            input.value = "";
+        }, 'click input.clear' : function () {
+            Todos.remove({});
+        }
+    });
+
+    document.addEventListener("deviceready", function(){
+       alert(device.platform);
+
+        if(navigator.notification){
+            navigator.notification.vibrate(6000);
+        }
+
+    }, false);
+    
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
+}
